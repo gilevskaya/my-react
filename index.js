a312abc36d35f4875d480afdd571c90923a79133
Alena Gilevskaya <alena@gilevskaya.com>
2020-04-11 21:05:32 -0700

    init

---
+++ index.js
@@ -0,0 +1,61 @@
+const ReactX = {
+  createElement,
+  render,
+};
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function render(element, container) {
+  const dom =
+    element.type == "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  const isProperty = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+
+  element.props.children.forEach((child) => render(child, dom));
+
+  container.appendChild(dom);
+}
+
+const element = ReactX.createElement(
+  "div",
+  { id: "test-id" },
+  ReactX.createElement("h1", null, "Hello"),
+  ReactX.createElement("div", null, "div-content")
+);
+// /** @jsx ReactX.createElement */
+// const element = (
+//   <div id="foo">
+//     <a>bar</a>
+//     <b />
+//   </div>
+// );
+
+const container = document.getElementById("root");
+ReactX.render(element, container);
