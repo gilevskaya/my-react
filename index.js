619f8dce1cd8e154007995f2a7cb08f8ce944875
Alena Gilevskaya <alena@gilevskaya.com>
2020-04-13 16:50:07 -0700

    workLoop + bug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ReactX = {
+const ReReact = {
   createElement,
   render,
 };
@@ -25,31 +25,117 @@ function createTextElement(text) {
   };
 }
 
-function render(element, container) {
+function createDom(fiber) {
+  console.log("createDom", fiber);
   const dom =
-    element.type == "TEXT_ELEMENT"
+    fiber.type == "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
 
-  element.props.children.forEach((child) => render(child, dom));
-
-  container.appendChild(dom);
+  return dom;
 }
 
-const element = ReactX.createElement(
+function commitRoot() {
+  commitWork(wipRoot.child);
+  wipRoot = null;
+}
+
+function commitWork(fiber) {
+  if (!fiber) {
+    return;
+  }
+  const domParent = fiber.parent.dom;
+  domParent.appendChild(fiber.dom);
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
+
+///
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+  nextUnitOfWork = wipRoot;
+}
+
+///
+let nextUnitOfWork = null;
+let wipRoot = null;
+
+function workLoop(deadline) {
+  console.log("workLoop", nextUnitOfWork);
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot();
+  }
+  requestIdleCallback(workLoop);
+}
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  console.log("performUtitOfWork", fiber);
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+
+    if (fiber.child) {
+      return fiber.child;
+    }
+    let nextFiber = fiber;
+    while (nextFiber) {
+      if (nextFiber.sibling) {
+        return nextFiber.sibling;
+      }
+      nextFiber = nextFiber.parent;
+    }
+  }
+}
+
+const element = ReReact.createElement(
   "div",
   { id: "test-id" },
-  ReactX.createElement("h1", null, "Hello"),
-  ReactX.createElement("div", null, "div-content")
+  ReReact.createElement("h1", null, "Hello"),
+  ReReact.createElement("div", null, "div-content")
 );
-// /** @jsx ReactX.createElement */
+// /** @jsx ReReact.createElement */
 // const element = (
 //   <div id="foo">
 //     <a>bar</a>
@@ -58,4 +144,4 @@ const element = ReactX.createElement(
 // );
 
 const container = document.getElementById("root");
-ReactX.render(element, container);
+ReReact.render(element, container);
